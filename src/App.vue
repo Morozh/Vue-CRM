2dc939d4853b4c325c2eddea3f6372b65558e573
Ruslan <rusl.trynin@yandex.com>
2021-02-05 14:48:58 +0500

    Настроил стандартные страницы + .editorconfig

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Main from '@/layouts/Main'
+import Auth from '@/layouts/Auth'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  components: {
+    Main,
+    Auth
+  },
+  computed: {
+    layout() {
+      return this.$route.meta.layout || 'Auth'
     }
   }
 }
+</script>
+
+<style lang="scss">
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/css/index.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,16 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'Auth' },
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: { layout: 'Main'}, // Кастомное свойство layout
+    component: () => import(/* webpackChunkName: "categories" */ '../views/Categories.vue')
   }
 ]
 
