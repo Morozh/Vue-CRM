11636392af41091179a7111a823381f01bedbf86
Ruslan <rusl.trynin@yandex.com>
2021-04-26 11:50:00 +0500

    Завершил раздел Сотрудники

--- src/views/Employee.vue
+++ src/views/Employee.vue
@@ -9,13 +9,306 @@
           <div class="employee-col__img">
             <img src="@/assets/img/avatar.png" alt="Аватар" />
           </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Сергей Солохин</div>
+            <div class="employee-col__info-role">Проектный менеджер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@sol</div>
+              <div class="employee-col__contacts-contact">sol@alamics.ru</div>
+            </div>
+          </div>
         </div>
         <div class="employee-col">
-          <div class="employee-col__name">Сергей</div>
-          <div class="employee-col__role">Менеджер</div>
-          <div class="employee-col__contacts">
-            <div class="employee-col__contacts-telegram">@asdsad</div>
-            <div class="employee-col__contacts-contact">asdasd@alamics.ru</div>
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Светлана Шевченко</div>
+            <div class="employee-col__info-role">Проектный менеджер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@shev</div>
+              <div class="employee-col__contacts-contact">shev@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Ян Болдырев</div>
+            <div class="employee-col__info-role">Дизайнер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@yan</div>
+              <div class="employee-col__contacts-contact">yan@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Влад Буич</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@buyich123</div>
+              <div class="employee-col__contacts-contact">buyich123@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Анна Левина</div>
+            <div class="employee-col__info-role">Менеджер / Директор</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@ann</div>
+              <div class="employee-col__contacts-contact">ann@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Алексей Манн</div>
+            <div class="employee-col__info-role">Ведущий разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@mann321</div>
+              <div class="employee-col__contacts-contact">mann321@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Елена Пономарева</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@len</div>
+              <div class="employee-col__contacts-contact">len@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Илья Пономарев</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@ilya</div>
+              <div class="employee-col__contacts-contact">ilya@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Евгений Лукин</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@ev</div>
+              <div class="employee-col__contacts-contact">ev@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Евгения Оловянникова</div>
+            <div class="employee-col__info-role">Проектный менеджер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@evg</div>
+              <div class="employee-col__contacts-contact">evg@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Евгений Лукин</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@ev</div>
+              <div class="employee-col__contacts-contact">ev@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Евгения Оловянникова</div>
+            <div class="employee-col__info-role">Проектный менеджер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@evg</div>
+              <div class="employee-col__contacts-contact">evg@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Руслан Акопян</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@akopy</div>
+              <div class="employee-col__contacts-contact">akopy@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Артем Валитов</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@val</div>
+              <div class="employee-col__contacts-contact">val@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Вадим Алексеенко</div>
+            <div class="employee-col__info-role">Разработчик</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@vad</div>
+              <div class="employee-col__contacts-contact">vad@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Артем Петренко</div>
+            <div class="employee-col__info-role">Менеджер / Дизайнер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@art</div>
+              <div class="employee-col__contacts-contact">art@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Кирилл Нагирняк</div>
+            <div class="employee-col__info-role">Дизайнер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@kir</div>
+              <div class="employee-col__contacts-contact">kir@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Светлана Власова</div>
+            <div class="employee-col__info-role">Дизайнер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@svet</div>
+              <div class="employee-col__contacts-contact">svet@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Марина Мальцева</div>
+            <div class="employee-col__info-role">HR специалист</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@mar</div>
+              <div class="employee-col__contacts-contact">mar@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Андрей Мельник</div>
+            <div class="employee-col__info-role">Дизайнер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@mel</div>
+              <div class="employee-col__contacts-contact">mel@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="employee-row">
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Оля Другова</div>
+            <div class="employee-col__info-role">Проектный менеджер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@olya</div>
+              <div class="employee-col__contacts-contact">olya@alamics.ru</div>
+            </div>
+          </div>
+        </div>
+        <div class="employee-col">
+          <div class="employee-col__img">
+            <img src="@/assets/img/avatar.png" alt="Аватар" />
+          </div>
+          <div class="employee-col__info">
+            <div class="employee-col__info-name">Алла Бояршинова</div>
+            <div class="employee-col__info-role">Бухгалтер</div>
+            <div class="employee-col__contacts">
+              <div class="employee-col__contacts-telegram">@alla</div>
+              <div class="employee-col__contacts-contact">alla@alamics.ru</div>
+            </div>
           </div>
         </div>
       </div>
@@ -43,7 +336,81 @@ export default {
   }
 
   .employee-col {
+    display: flex;
+    align-items: center;
     padding: 20px;
-    flex-grow: 1;
+    flex: 1 1 50%;
+    color: #292929;
+    font-family: 'Roboto Bold', sans-serif;
+    font-size: 20px;
+    font-weight: 500;
+
+    &__img {
+      width: max(140px, 140px);
+      height: max(140px, 140px);
+      margin-right: 20px;
+      border-radius: 50%;
+      border: 1px solid #dddddd;
+      overflow: hidden;
+    }
+
+    &__info {
+      &-name {
+        font-family: 'Roboto Medium', sans-serif;
+        font-size: 20px;
+        font-weight: 500;
+        // margin: 0 0 8px 0;
+      }
+
+      &-role {
+        font-size: 16px;
+        color: #adadad;
+      }
+    }
+
+    &__contacts {
+      font-size: 14px;
+      line-height: 18px;
+      margin: 8px 0 0 0;
+
+      &-telegram {
+        position: relative;
+        padding: 0 0 0 32px;
+        margin: 0 0 12px 0;
+
+        &:before {
+          content: '';
+          position: absolute;
+          background-image: url('../assets/img/icons/telegram.svg');
+          background-size: cover;
+          background-position: center center;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          width: 24px;
+          height: 24px;
+          margin: 0 8px 0 0;
+        }
+      }
+
+      &-contact {
+        position: relative;
+        padding: 0 0 0 32px;
+
+        &:before {
+          content: '';
+          position: absolute;
+          background-image: url('../assets/img/icons/gmail.svg');
+          background-size: cover;
+          background-position: center center;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          width: 24px;
+          height: 24px;
+          margin: 0 8px 0 0;
+        }
+      }
+    }
   }
 </style>
