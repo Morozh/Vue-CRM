94d10b175d3013778e4b9c2c3eb41728ca5cc683
Ruslan <rusl.trynin@yandex.com>
2021-05-08 17:16:20 +0500

    Доделал раздел о системе

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>О системе</div>
+  <div class="about">
+    <div class="page-title">
+      <h3 class="title">О системе &laquo;Galago Design CRM system&raquo;</h3>
+    </div>
+    <section class="about-content">
+      <div class="about-content__info">
+        <p class="about-content__info-title">1. Что это за проект?</p>
+        <p class="about-content__info-text">Это корпоративный портал с элементами CRM системы, предназначенный для облегчения работы и ускорения коммуникации наших сотрудников.</p>
+      </div>
+      <div class="about-content__info">
+        <p class="about-content__info-title">2. Для кого предназначена система?</p>
+        <p class="about-content__info-text">Система предназначена исключительно для внутреннего использования сотрудниками компании Galago Design.</p>
+      </div>
+      <div class="about-content__info">
+        <p class="about-content__info-title">3. Могу ли я влиять на развитие проекта?</p>
+        <p class="about-content__info-text">Любой сотрудник компании имеет право предложить новые идеи для развития и поддержки системы. Для этого обратитесь к одному из ведущих менеджеров: Сергей Солохин, Светлана Шевченко.</p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -7,3 +25,19 @@ export default {
   name: 'About'
 }
 </script>
+
+<style lang="scss" scoped>
+  .about-content {
+    &__info {
+      &-title {
+        font-family: 'Roboto Medium', sans-serif;
+        font-size: 18px;
+        font-weight: 500;
+      }
+      
+      &-text {
+        color: #2f2f2f;
+      }
+    }
+  }
+</style>
